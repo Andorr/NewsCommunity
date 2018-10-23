b3f01decccefac61a9b3c9115eb0475ad0285b85
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-10-24 00:22:11 +0200

    Removed logs of .env

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const http = require('http');
 const wss = require('./api/helpers/ws');
 require('dotenv').config();
 
-console.log(process.env.MONGODB_PASSWORD);
-console.log(process.env.JWT_KEY);
-
 const port = process.env.PORT || 8080;
 
 const server = http.createServer(require('./app'));
