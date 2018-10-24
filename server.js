0ac1dbf39613b5dfe5825cfc97d2de5cfe2deff4
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-10-24 10:11:35 +0200

    added process.env.port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const wss = require('./api/helpers/ws');
 const dotenv = require('dotenv');
 dotenv.config();
 
-let port: any = 8080;
+let port: any = process.env.PORT || 8080;
 
 const server = http.createServer(require('./app'));
 wss.init(server);
